057b2163b1c337e3ac8d4c96db9ac09da7adb982
Simon Saunders <simon.saunders@ntlworld.com>
2020-04-26 12:54:32 +0100

    abbrev_auto.pl fixed to handle mixed case

--- abbrev_auto.pl
+++ abbrev_auto.pl
@@ -22,12 +22,18 @@ common_prefix_length1([C|Chars1], [C|Chars2], Length, L1):-
 	L2 is L1 + 1,
 	common_prefix_length1(Chars1, Chars2, Length, L2).
 
+to_upper_case([], []):-!.
+to_upper_case([String|S], [Upper_case|U]):-
+    string_upper(String, Upper_case),
+    to_upper_case(S, U).
+
 process_line(""):-
     nl,
     !.
 process_line(Line):-
     split_string(Line, "\s\t", "\s\t", Day_names),
-    minimum_abbreviation_length(Day_names, Length),
+    to_upper_case(Day_names, Upper),
+    minimum_abbreviation_length(Upper, Length),
     writef('%w %w\n', [Length, Line]).
 
 process_stream(Stream):-
