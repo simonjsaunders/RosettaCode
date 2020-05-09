794380412b61d05ff513fc563e1e0cd73a72bbf1
Simon Saunders <simon.saunders@ntlworld.com>
2020-05-09 18:13:35 +0100

    Improved output of C++ solutions for 'Stirling numbers of the 1st/2nd kind'

--- stirling1.cpp
+++ stirling1.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
@@ -6,16 +7,14 @@
 
 using integer = mpz_class;
 
-class unsigned_stirling1
-{
+class unsigned_stirling1 {
 public:
     integer get(int n, int k);
 private:
     std::map<std::pair<int, int>, integer> cache_;
 };
 
-integer unsigned_stirling1::get(int n, int k)
-{
+integer unsigned_stirling1::get(int n, int k) {
     if (k == 0)
         return n == 0 ? 1 : 0;
     if (k > n)
@@ -29,29 +28,27 @@ integer unsigned_stirling1::get(int n, int k)
     return s;
 }
 
-void print_stirling_numbers(unsigned_stirling1& s1, int n)
-{
-    for (int i = 0; i <= n; ++i)
-    {
+void print_stirling_numbers(unsigned_stirling1& s1, int n) {
+    std::cout << "Unsigned Stirling numbers of the first kind:\nn/k";
+    for (int j = 0; j <= n; ++j) {
+        std::cout << std::setw(j == 0 ? 2 : 10) << j;
+    }
+    std::cout << '\n';
+    for (int i = 0; i <= n; ++i) {
+        std::cout << std::setw(2) << i << ' ';
         for (int j = 0; j <= i; ++j)
-            std::cout << std::setw(10) << s1.get(i, j);
+            std::cout << std::setw(j == 0 ? 2 : 10) << s1.get(i, j);
         std::cout << '\n';
     }
 }
 
-int main()
-{
+int main() {
     unsigned_stirling1 s1;
-    std::cout << "Unsigned Stirling numbers of the first kind:\n";
     print_stirling_numbers(s1, 12);
     std::cout << "Maximum value of S1(n,k) where n == 100:\n";
     integer max = 0;
     for (int k = 0; k <= 100; ++k)
-    {
-        integer s = s1.get(100, k);
-        if (s > max)
-            max = s;
-    }
+        max = std::max(max, s1.get(100, k));
     std::cout << max << '\n';
     return 0;
 }
--- stirling2.cpp
+++ stirling2.cpp
@@ -6,16 +6,14 @@
 
 using integer = mpz_class;
 
-class stirling2
-{
+class stirling2 {
 public:
     integer get(int n, int k);
 private:
     std::map<std::pair<int, int>, integer> cache_;
 };
 
-integer stirling2::get(int n, int k)
-{
+integer stirling2::get(int n, int k) {
     if (k == n)
         return 1;
     if (k == 0 || k > n)
@@ -29,29 +27,27 @@ integer stirling2::get(int n, int k)
     return s;
 }
 
-void print_stirling_numbers(stirling2& s2, int n)
-{
-    for (int i = 0; i <= n; ++i)
-    {
+void print_stirling_numbers(stirling2& s2, int n) {
+    std::cout << "Stirling numbers of the second kind:\nn/k";
+    for (int j = 0; j <= n; ++j) {
+        std::cout << std::setw(j == 0 ? 2 : 8) << j;
+    }
+    std::cout << '\n';
+    for (int i = 0; i <= n; ++i) {
+        std::cout << std::setw(2) << i << ' ';
         for (int j = 0; j <= i; ++j)
-            std::cout << std::setw(8) << s2.get(i, j);
+            std::cout << std::setw(j == 0 ? 2 : 8) << s2.get(i, j);
         std::cout << '\n';
     }
 }
 
-int main()
-{
+int main() {
     stirling2 s2;
-    std::cout << "Stirling numbers of the second kind:\n";
     print_stirling_numbers(s2, 12);
     std::cout << "Maximum value of S2(n,k) where n == 100:\n";
     integer max = 0;
     for (int k = 0; k <= 100; ++k)
-    {
-        integer s = s2.get(100, k);
-        if (s > max)
-            max = s;
-    }
+        max = std::max(max, s2.get(100, k));
     std::cout << max << '\n';
     return 0;
 }
