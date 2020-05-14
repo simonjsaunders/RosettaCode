4be2574aceb6367a40ed0b9c3169a24d34483b47
Simon Saunders <simon.saunders@ntlworld.com>
2020-05-14 13:42:18 +0100

    More efficient implementation of n-smooth number generator

--- pierpont_primes/pierpont_primes.cpp
+++ pierpont_primes/pierpont_primes.cpp
@@ -1,51 +1,56 @@
 #include <cassert>
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
-#include <set>
 #include <vector>
 #include <gmpxx.h>
 
 using big_int = mpz_class;
 
-bool is_prime(const big_int& n)
-{
+bool is_prime(const big_int& n) {
     return mpz_probab_prime_p(n.get_mpz_t(), 25);
 }
 
-class n_smooth_generator
-{
+template <typename integer>
+class n_smooth_generator {
 public:
-    explicit n_smooth_generator(int n);
-    big_int next();
+    explicit n_smooth_generator(size_t n);
+    integer next();
+    size_t size() const {
+        return results_.size();
+    }
 private:
-    std::vector<int> primes_;
-    std::set<big_int> numbers_;
+    std::vector<size_t> primes_;
+    std::vector<size_t> index_;
+    std::vector<integer> results_;
+    std::vector<integer> queue_;
 };
 
-n_smooth_generator::n_smooth_generator(int n)
-{
-    for (int p = 2; p <= n; ++p)
-    {
-        if (is_prime(p))
+template <typename integer>
+n_smooth_generator<integer>::n_smooth_generator(size_t n) {
+    for (size_t p = 2; p <= n; ++p) {
+        if (is_prime(p)) {
             primes_.push_back(p);
+            queue_.push_back(p);
+        }
     }
-    numbers_.insert(1);
+    index_.assign(primes_.size(), 0);
+    results_.push_back(1);
 }
 
-big_int n_smooth_generator::next()
-{
-    assert(!numbers_.empty());
-    big_int result = *numbers_.begin();
-    numbers_.erase(numbers_.begin());
-    for (auto prime : primes_)
-        numbers_.insert(result * prime);
-    return result;
+template <typename integer>
+integer n_smooth_generator<integer>::next() {
+    integer last = results_.back();
+    for (size_t i = 0, n = primes_.size(); i < n; ++i) {
+        if (queue_[i] == last)
+            queue_[i] = results_[++index_[i]] * primes_[i];
+    }
+    results_.push_back(*min_element(queue_.begin(), queue_.end()));
+    return last;
 }
 
-void print_vector(const std::vector<big_int>& numbers)
-{
-    for (size_t i = 0, n = numbers.size(); i < n; ++i)
-    {
+void print_vector(const std::vector<big_int>& numbers) {
+    for (size_t i = 0, n = numbers.size(); i < n; ++i) {
         std::cout << std::setw(9) << numbers[i];
         if ((i + 1) % 10 == 0)
             std::cout << '\n';
@@ -53,27 +58,23 @@ void print_vector(const std::vector<big_int>& numbers)
     std::cout << '\n';
 }
 
-int main()
-{
+int main() {
     const int max = 250;
     std::vector<big_int> first, second;
     int count1 = 0;
     int count2 = 0;
-    n_smooth_generator smooth_gen(3);
+    n_smooth_generator<big_int> smooth_gen(3);
     big_int p1, p2;
 
-    while (count1 < max || count2 < max)
-    {
+    while (count1 < max || count2 < max) {
         big_int n = smooth_gen.next();
-        if (count1 < max && is_prime(n + 1))
-        {
+        if (count1 < max && is_prime(n + 1)) {
             p1 = n + 1;
             if (first.size() < 50)
                 first.push_back(p1);
             ++count1;
         }
-        if (count2 < max && is_prime(n - 1))
-        {
+        if (count2 < max && is_prime(n - 1)) {
             p2 = n - 1;
             if (second.size() < 50)
                 second.push_back(p2);
@@ -88,6 +89,5 @@ int main()
     
     std::cout << "250th Pierpont prime of the first kind: " << p1 << '\n';
     std::cout << "250th Pierpont prime of the second kind: " << p2 << '\n';
-
     return 0;
 }
