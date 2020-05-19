ca773dfce3c37fdaabbef7fe29a858e74c2ec43a
Simon Saunders <simon.saunders@ntlworld.com>
2020-05-19 10:36:39 +0100

    Changed formatting

--- sexy_primes/sexy_primes.cpp
+++ sexy_primes/sexy_primes.cpp
@@ -4,8 +4,7 @@
 #include "../library/circular_buffer.h"
 #include "../library/sieve_of_eratosthenes.h"
 
-int main()
-{
+int main() {
     using std::cout;
     using std::vector;
 
@@ -23,26 +22,20 @@ int main()
     vector<circular_buffer<vector<int>>> groups;
     for (size_t i = 0; i < max_group_size; ++i)
         groups.emplace_back(max_groups);
-
     int unsexy_count = 0;
     circular_buffer<int> unsexy_primes(max_unsexy);
 
-    for (int p = 2; p < max; )
-    {
-        if (!sieve.is_prime(p + diff) && (p - diff < 2 || !sieve.is_prime(p - diff)))
-        {
+    for (int p = 2; p < max; ) {
+        if (!sieve.is_prime(p + diff) && (p - diff < 2 || !sieve.is_prime(p - diff))) {
             // if p + diff and p - diff aren't prime then p can't be sexy
             ++unsexy_count;
             unsexy_primes.push_back(p);
-        }
-        else
-        {
+        } else {
             // find the groups of sexy primes that begin with p
             int group_size = 1;
             vector<int> group;
             group.push_back(p);
-            for (int i = 1; i < max_group_size; ++i)
-            {
+            for (int i = 1; i < max_group_size; ++i) {
                 int next_p = p + i * diff;
                 if (next_p >= max || !sieve.is_prime(next_p))
                     break;
@@ -59,15 +52,12 @@ int main()
             ++p;
     }
 
-    for (int size = 1; size < max_group_size; ++size)
-    {
+    for (int size = 1; size < max_group_size; ++size) {
         cout << "number of groups of size " << size + 1 << " is " << group_count[size] << '\n';
         cout << "last " << groups[size].size() << " groups of size " << size + 1 << ":";
-        for (const vector<int>& group : groups[size])
-        {
+        for (const vector<int>& group : groups[size]) {
             cout << " (";
-            for (size_t i = 0; i < group.size(); ++i)
-            {
+            for (size_t i = 0; i < group.size(); ++i) {
                 if (i > 0)
                     cout << ' ';
                 cout << group[i];
@@ -76,12 +66,10 @@ int main()
         }
         cout << "\n\n";
     }
-
     cout << "number of unsexy primes is " << unsexy_count << '\n';
     cout << "last " << unsexy_primes.size() << " unsexy primes:";
     for (int prime : unsexy_primes)
         cout << ' ' << prime;
     cout << '\n';
-
     return 0;
 }
