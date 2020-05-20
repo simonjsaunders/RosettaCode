bd7ff7749e68fb74c35e6c382e61bceb6c86eb7d
Simon Saunders <simon.saunders@ntlworld.com>
2020-05-20 14:17:46 +0100

    Updated prime_conspiracy2.rs to produce same output as prime_conspiracy.rs

--- prime_conspiracy/prime_conspiracy2.rs
+++ prime_conspiracy/prime_conspiracy2.rs
@@ -1,11 +1,10 @@
 // [dependencies]
 // primal = "0.2"
 
-fn main() {
+fn compute_transitions(limit : usize) {
     use std::collections::BTreeMap;
     let mut transitions = BTreeMap::new();
     let mut prev = 0;
-    let limit = 100_000_000;
     for n in primal::Primes::all().take(limit) {
         let digit = n % 10;
         if prev != 0 {
@@ -23,4 +22,10 @@ fn main() {
         let freq = 100.0*(*c as f32)/(limit as f32);
         println!("{} -> {}: count = {:7}, frequency = {:.2} %", from, to, c, freq);
     }
+}
+
+fn main() {
+    compute_transitions(1000000);
+    println!();
+    compute_transitions(100000000);
 }
