06673e24a83ed63ed2fbfb6217f623f014516b53
Simon Saunders <simon.saunders@ntlworld.com>
2020-05-25 22:35:07 +0100

    Code simplified

--- stirling2/stirling_numbers2.c
+++ stirling2/stirling_numbers2.c
@@ -3,44 +3,37 @@
 #include <stdlib.h>
 
 typedef struct stirling_cache_tag {
-    int n;
+    int max;
     int* values;
 } stirling_cache;
 
-bool stirling_cache_create(stirling_cache* sc, int n) {
-    int* values = calloc(n * (n + 1)/2, sizeof(int));
+int stirling_number2(stirling_cache* sc, int n, int k) {
+    if (k == n)
+        return 1;
+    if (k == 0 || k > n || n > sc->max)
+        return 0;
+    return sc->values[n*(n-1)/2 + k - 1];
+}
+
+bool stirling_cache_create(stirling_cache* sc, int max) {
+    int* values = calloc(max * (max + 1)/2, sizeof(int));
     if (values == NULL)
         return false;
-    sc->n = n;
+    sc->max = max;
     sc->values = values;
+    for (int n = 1; n <= max; ++n) {
+        for (int k = 1; k < n; ++k) {
+            int s1 = stirling_number2(sc, n - 1, k - 1);
+            int s2 = stirling_number2(sc, n - 1, k);
+            values[n*(n-1)/2 + k - 1] = s1 + s2 * k;
+        }
+    }
     return true;
 }
 
 void stirling_cache_destroy(stirling_cache* sc) {
     free(sc->values);
     sc->values = NULL;
-}
-
-int* stirling_cache_get(stirling_cache* sc, int n, int k) {
-    return (n > sc->n) ? NULL : &sc->values[n*(n-1)/2 + k - 1];
-}
-
-int stirling_number2(stirling_cache* sc, int n, int k) {
-    if (k == n)
-        return 1;
-    if (k == 0 || k > n)
-        return 0;
-    int* value = stirling_cache_get(sc, n, k);
-    if (value == NULL) {
-        fprintf(stderr, "Cache size is too small\n");
-        exit(1);
-    }
-    if (*value == 0) {
-        int s1 = stirling_number2(sc, n - 1, k - 1);
-        int s2 = stirling_number2(sc, n - 1, k);
-        *value = 1 + s1 + s2 * k;
-    }
-    return *value - 1;
 }
 
 void print_stirling_numbers(stirling_cache* sc, int max) {
