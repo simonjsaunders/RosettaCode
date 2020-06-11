879764ea3c4ef22f65ed291f4fa3f11ba640c70a
Simon Saunders <simon.saunders@ntlworld.com>
2020-06-11 13:44:21 +0100

    Made code a bit neater

--- koch_curve/koch_curve.cpp
+++ koch_curve/koch_curve.cpp
@@ -15,24 +15,19 @@ std::vector<point> koch_next(const std::vector<point>& points) {
     std::vector<point> output(4*(size - 1) + 1);
     double x0, y0, x1, y1;
     size_t j = 0;
-    for (size_t i = 0; i + 1 < size; ++i, j += 4) {
+    for (size_t i = 0; i + 1 < size; ++i) {
         x0 = points[i].x;
         y0 = points[i].y;
         x1 = points[i + 1].x;
         y1 = points[i + 1].y;
         double dy = y1 - y0;
         double dx = x1 - x0;
-        output[j].x = x0;
-        output[j].y = y0;
-        output[j + 1].x = x0 + dx/3;
-        output[j + 1].y = y0 + dy/3;
-        output[j + 2].x = x0 + dx/2 - dy * sqrt3_2/3;
-        output[j + 2].y = y0 + dy/2 + dx * sqrt3_2/3;
-        output[j + 3].x = x0 + 2 * dx/3;
-        output[j + 3].y = y0 + 2 * dy/3;
+        output[j++] = {x0, y0};
+        output[j++] = {x0 + dx/3, y0 + dy/3};
+        output[j++] = {x0 + dx/2 - dy * sqrt3_2/3, y0 + dy/2 + dx * sqrt3_2/3};
+        output[j++] = {x0 + 2 * dx/3, y0 + 2 * dy/3};
     }
-    output[j].x = x1;
-    output[j].y = y1;
+    output[j] = {x1, y1};
     return output;
 }
 
@@ -44,7 +39,7 @@ std::vector<point> koch_points(int size, int iterations) {
         {x, y},
         {x + length/2, y + length * sqrt3_2},
         {x + length, y},
-        {x, y},
+        {x, y}
     };
     for (int i = 0; i < iterations; ++i)
         points = koch_next(points);
@@ -55,14 +50,10 @@ void koch_curve_svg(std::ostream& out, int size, int iterations) {
     out << "<svg xmlns='http://www.w3.org/2000/svg' width='"
         << size << "' height='" << size << "'>\n";
     out << "<rect width='100%' height='100%' fill='black'/>\n";
-    out << "<path stroke-width='1' stroke='white' fill='none' d='M ";
+    out << "<path stroke-width='1' stroke='white' fill='none' d='";
     auto points(koch_points(size, iterations));
-    size_t n = points.size();
-    if (n > 0) {
-        out << points[0].x << ',' << points[0].y << ' ';
-        for (size_t i = 1; i < n; ++i)
-            out << "L " << points[i].x << ',' << points[i].y << '\n';
-    }
+    for (size_t i = 0, n = points.size(); i < n; ++i)
+        out << (i == 0 ? "M" : "L") << points[i].x << ',' << points[i].y << '\n';
     out << "z'/>\n</svg>\n";
 }
 
