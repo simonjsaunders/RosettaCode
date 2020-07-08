a6d8562dbcb2dd8d517e28ee346658efe6890702
Simon Saunders <simon.saunders@ntlworld.com>
2020-07-08 14:28:10 +0100

    Improved performance by not using sieve

--- smarandache/smarandache.cpp
+++ smarandache/smarandache.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdint>
-#include "prime_sieve.hpp"
 
 using integer = uint32_t;
 
@@ -18,30 +17,44 @@ integer next_prime_digit_number(integer n) {
     }
 }
 
+bool is_prime(integer n) {
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    if (n % 3 == 0)
+        return n == 3;
+    for (integer p = 5; p * p <= n; p += 4) {
+        if (n % p == 0)
+            return false;
+        p += 2;
+        if (n % p == 0)
+            return false;
+    }
+    return true;
+}
+
 int main() {
     const integer limit = 10000000;
-    prime_sieve sieve(limit);
     integer n = 0, n1 = 0, n2 = 0, n3 = 0;
     std::cout << "First 25 SPDS primes:\n";
-    for (int i = 0; ; ) {
+    for (int i = 0; n < limit; ) {
         n = next_prime_digit_number(n);
-        if (n >= limit)
-            break;
-        if (sieve.is_prime(n)) {
-            if (i < 25) {
-                if (i > 0)
-                    std::cout << ", ";
-                std::cout << n;
-            }
-            else if (i == 25)
-                std::cout << '\n';
-            ++i;
-            if (i == 100)
-                n1 = n;
-            else if (i == 1000)
-                n2 = n;
-            n3 = n;
+        if (!is_prime(n))
+            continue;
+        if (i < 25) {
+            if (i > 0)
+                std::cout << ", ";
+            std::cout << n;
         }
+        else if (i == 25)
+            std::cout << '\n';
+        ++i;
+        if (i == 100)
+            n1 = n;
+        else if (i == 1000)
+            n2 = n;
+        n3 = n;
     }
     std::cout << "Hundredth SPDS prime: " << n1 << '\n';
     std::cout << "Thousandth SPDS prime: " << n2 << '\n';
--- smarandache/spds.c
+++ smarandache/spds.c
@@ -1,9 +1,6 @@
-#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "prime_sieve.h"
 
 uint32_t next_prime_digit_number(uint32_t n) {
     if (n == 0)
@@ -19,36 +16,45 @@ uint32_t next_prime_digit_number(uint32_t n) {
     }
 }
 
+bool is_prime(uint32_t n) {
+    if (n < 2)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    if (n % 3 == 0)
+        return n == 3;
+    for (uint32_t p = 5; p * p <= n; p += 4) {
+        if (n % p == 0)
+            return false;
+        p += 2;
+        if (n % p == 0)
+            return false;
+    }
+    return true;
+}
+
 int main() {
     const uint32_t limit = 10000000;
-    prime_sieve s = { 0 };
-    if (!prime_sieve_create(&s, limit)) {
-        fprintf(stderr, "Out of memory\n");
-        return 1;
-    }
     uint32_t n = 0, n1 = 0, n2 = 0, n3 = 0;
     printf("First 25 SPDS primes:\n");
-    for (int i = 0; ; ) {
+    for (int i = 0; n < limit; ) {
         n = next_prime_digit_number(n);
-        if (n >= limit)
-            break;
-        if (is_prime(&s, n)) {
-            if (i < 25) {
-                if (i > 0)
-                    printf(", ");
-                printf("%u", n);
-            }
-            else if (i == 25)
-                printf("\n");
-            ++i;
-            if (i == 100)
-                n1 = n;
-            else if (i == 1000)
-                n2 = n;
-            n3 = n;
+        if (!is_prime(n))
+            continue;
+        if (i < 25) {
+            if (i > 0)
+                printf(", ");
+            printf("%u", n);
         }
+        else if (i == 25)
+            printf("\n");
+        ++i;
+        if (i == 100)
+            n1 = n;
+        else if (i == 1000)
+            n2 = n;
+        n3 = n;
     }
-    prime_sieve_destroy(&s);
     printf("Hundredth SPDS prime: %u\n", n1);
     printf("Thousandth SPDS prime: %u\n", n2);
     printf("Largest SPDS prime less than %u: %u\n", limit, n3);
