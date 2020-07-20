82058cfdc8a4ea3d9728d8c93bc891f3412af749
Simon Saunders <simon.saunders@ntlworld.com>
2020-07-20 22:21:39 +0100

    Made some changes to matrix class

--- gauss_jordan/gauss_jordan.cpp
+++ gauss_jordan/gauss_jordan.cpp
@@ -12,36 +12,24 @@ public:
     matrix(size_t rows, size_t columns, scalar_type value)
         : rows_(rows), columns_(columns), elements_(rows * columns, value) {}
 
-    matrix(size_t rows, size_t columns,
-        const std::initializer_list<std::initializer_list<scalar_type>>& values)
+    matrix(size_t rows, size_t columns, std::initializer_list<scalar_type> values)
         : rows_(rows), columns_(columns), elements_(rows * columns) {
-        assert(values.size() <= rows_);
-        size_t i = 0;
-        for (const auto& row : values) {
-            assert(row.size() <= columns_);
-            std::copy(begin(row), end(row), row_data(i++));
-        }
+        assert(values.size() <= rows_ * columns_);
+        std::copy(values.begin(), values.end(), elements_.begin());
     }
 
     size_t rows() const { return rows_; }
     size_t columns() const { return columns_; }
 
-    scalar_type* row_data(size_t row) {
+    scalar_type& operator()(size_t row, size_t column) {
         assert(row < rows_);
-        return &elements_[row * columns_];
+        assert(column < columns_);
+        return elements_[row * columns_ + column];
     }
-    const scalar_type* row_data(size_t row) const {
+    const scalar_type& operator()(size_t row, size_t column) const {
         assert(row < rows_);
-        return &elements_[row * columns_];
-    }
-
-    const scalar_type& at(size_t row, size_t column) const {
         assert(column < columns_);
-        return row_data(row)[column];
-    }
-    scalar_type& at(size_t row, size_t column) {
-        assert(column < columns_);
-        return row_data(row)[column];
+        return elements_[row * columns_ + column];
     }
 private:
     size_t rows_;
@@ -58,12 +46,9 @@ matrix<scalar_type> product(const matrix<scalar_type>& a,
     size_t n = a.columns();
     matrix<scalar_type> c(arows, bcolumns);
     for (size_t i = 0; i < arows; ++i) {
-        const auto* aptr = a.row_data(i);
-        auto* cptr = c.row_data(i);
         for (size_t j = 0; j < n; ++j) {
-            const auto* bptr = b.row_data(j);
             for (size_t k = 0; k < bcolumns; ++k)
-                cptr[k] += aptr[j] * bptr[k];
+                c(i, k) += a(i, j) * b(j, k);
         }
     }
     return c;
@@ -73,7 +58,7 @@ template <typename scalar_type>
 void swap_rows(matrix<scalar_type>& m, size_t i, size_t j) {
     size_t columns = m.columns();
     for (size_t column = 0; column < columns; ++column)
-        std::swap(m.at(i, column), m.at(j, column));
+        std::swap(m(i, column), m(j, column));
 }
 
 // Convert matrix to reduced row echelon form
@@ -83,7 +68,7 @@ void rref(matrix<scalar_type>& m) {
     size_t columns = m.columns();
     for (size_t row = 0, lead = 0; row < rows && lead < columns; ++row, ++lead) {
         size_t i = row;
-        while (m.at(i, lead) == 0) {
+        while (m(i, lead) == 0) {
             if (++i == rows) {
                 i = row;
                 if (++lead == columns)
@@ -91,17 +76,17 @@ void rref(matrix<scalar_type>& m) {
             }
         }
         swap_rows(m, i, row);
-        if (m.at(row, lead) != 0) {
-            scalar_type f = m.at(row, lead);
+        if (m(row, lead) != 0) {
+            scalar_type f = m(row, lead);
             for (size_t column = 0; column < columns; ++column)
-                m.at(row, column) /= f;
+                m(row, column) /= f;
         }
         for (size_t j = 0; j < rows; ++j) {
             if (j == row)
                 continue;
-            scalar_type f = m.at(j, lead);
+            scalar_type f = m(j, lead);
             for (size_t column = 0; column < columns; ++column)
-                m.at(j, column) -= f * m.at(row, column);
+                m(j, column) -= f * m(row, column);
         }
     }
 }
@@ -112,34 +97,35 @@ matrix<scalar_type> inverse(const matrix<scalar_type>& m) {
     size_t rows = m.rows();
     matrix<scalar_type> tmp(rows, 2 * rows, 0);
     for (size_t row = 0; row < rows; ++row) {
-        std::copy_n(m.row_data(row), rows, tmp.row_data(row));
-        tmp.at(row, row + rows) = 1;
+        for (size_t column = 0; column < rows; ++column)
+            tmp(row, column) = m(row, column);
+        tmp(row, row + rows) = 1;
     }
     rref(tmp);
     matrix<scalar_type> inv(rows, rows);
-    for (size_t row = 0; row < rows; ++row)
-        std::copy_n(tmp.row_data(row) + rows, rows, inv.row_data(row));
+    for (size_t row = 0; row < rows; ++row) {
+        for (size_t column = 0; column < rows; ++column)
+            inv(row, column) = tmp(row, column + rows);
+    }
     return inv;
 }
 
 template <typename scalar_type>
-void print(std::ostream& out, const matrix<scalar_type>& a) {
-    size_t rows = a.rows(), columns = a.columns();
+void print(std::ostream& out, const matrix<scalar_type>& m) {
+    size_t rows = m.rows(), columns = m.columns();
     out << std::fixed << std::setprecision(4);
     for (size_t row = 0; row < rows; ++row) {
         for (size_t column = 0; column < columns; ++column) {
             if (column > 0)
                 out << ' ';
-            out << std::setw(7) << a.at(row, column);
+            out << std::setw(7) << m(row, column);
         }
         out << '\n';
     }
 }
 
 int main() {
-    matrix<double> m(3, 3, {{ 2, -1,  0},
-                            {-1,  2, -1},
-                            { 0, -1,  2}});
+    matrix<double> m(3, 3, {2, -1, 0, -1, 2, -1, 0, -1, 2});
     std::cout << "Matrix:\n";
     print(std::cout, m);
     auto inv(inverse(m));
