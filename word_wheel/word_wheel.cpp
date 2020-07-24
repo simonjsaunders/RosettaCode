a5f319d0ab84d25db6037a41ea9cf75c90ed8bd5
Simon Saunders <simon.saunders@ntlworld.com>
2020-07-24 17:02:36 +0100

    Implement optional part 2 of the task

--- word_wheel/word_wheel.cpp
+++ word_wheel/word_wheel.cpp
@@ -1,9 +1,11 @@
 #include <array>
+#include <cassert>
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <string>
 #include <vector>
+#include <boost/program_options.hpp>
 
 // A multiset specialized for strings consisting of lowercase
 // letters ('a' to 'z').
@@ -49,33 +51,138 @@ std::string join(iterator begin, iterator end, separator sep) {
     return result;
 }
 
-int main(int argc, char** argv) {
-    const int min_length = 3;
-    const char* letters = "ndeokgelw";
-    const char central_letter = 'k';
-    const char* dict(argc == 2 ? argv[1] : "unixdict.txt");
-    std::ifstream in(dict);
-    if (!in) {
-        std::cerr << "Cannot open file " << dict << '\n';
-        return 1;
-    }
-    letterset set(letters);
+using dictionary = std::vector<std::pair<std::string, letterset>>;
+
+dictionary load_dictionary(const std::string& filename, int min_length) {
+    std::ifstream in(filename);
+    if (!in)
+        throw std::runtime_error("Cannot open file " + filename);
     std::string word;
-    std::map<size_t, std::vector<std::string>> words;
+    dictionary result;
     while (getline(in, word)) {
         if (word.size() < min_length)
             continue;
-        letterset subset(word);
-        if (subset.count(central_letter) > 0 && set.contains(subset)) {
-            words[word.size()].push_back(word);
-        }
+        letterset set(word);
+        if (set.is_valid())
+            result.emplace_back(word, set);
     }
+    return result;
+}
+
+void word_wheel(const dictionary& dict, const std::string& letters,
+                char central_letter)  {
+    letterset set(letters);
+    if (central_letter == 0 && !letters.empty())
+        central_letter = letters.at(letters.size()/2);
+    std::map<size_t, std::vector<std::string>> words;
+    for (const auto& pair : dict) {
+        const auto& word = pair.first;
+        const auto& subset = pair.second;
+        if (subset.count(central_letter) > 0 && set.contains(subset))
+            words[word.size()].push_back(word);
+    }
+    size_t total = 0;
     for (const auto& p : words) {
         const auto& v = p.second;
         auto n = v.size();
+        total += n;
         std::cout << "Found " << n << " " << (n == 1 ? "word" : "words")
             << " of length " << p.first << ": "
             << join(v.begin(), v.end(), ", ") << '\n';
     }
-    return 0;
+    std::cout << "Number of words found: " << total << '\n';
+}
+
+void find_max_word_count(const dictionary& dict, int word_length) {
+    size_t max_count = 0;
+    std::vector<std::pair<std::string, char>> max_words;
+    for (const auto& pair : dict) {
+        const auto& word = pair.first;
+        if (word.size() != word_length)
+            continue;
+        const auto& set = pair.second;
+        dictionary subsets;
+        for (const auto& p : dict) {
+            if (set.contains(p.second))
+                subsets.push_back(p);
+        }
+        std::array<bool, 26> done{false};
+        for (size_t index = 0; index < word_length; ++index) {
+            char central_letter = word[index];
+            assert(central_letter >= 'a' && central_letter <= 'z');
+            if (done[central_letter - 'a'])
+                continue;
+            done[central_letter - 'a'] = true;
+            size_t count = 0;
+            for (const auto& p : subsets) {
+                const auto& subset = p.second;
+                if (subset.count(central_letter) > 0)
+                    ++count;
+            }
+            if (count > max_count) {
+                max_words.clear();
+                max_count = count;
+            }
+            if (count == max_count)
+                max_words.emplace_back(word, central_letter);
+        }
+    }
+    std::cout << "Maximum word count: " << max_count << '\n';
+    std::cout << "Words of " << word_length << " letters producing this count:\n";
+    for (const auto& pair : max_words)
+        std::cout << pair.first << " with central letter " << pair.second << '\n';
+}
+
+constexpr const char* option_filename = "filename";
+constexpr const char* option_wheel = "wheel";
+constexpr const char* option_central = "central";
+constexpr const char* option_min_length = "min-length";
+constexpr const char* option_part2 = "part2";
+
+int main(int argc, char** argv) {
+    const int word_length = 9;
+    int min_length = 3;
+    std::string letters = "ndeokgelw";
+    std::string filename = "unixdict.txt";
+    char central_letter = 0;
+    bool do_part2 = false;
+    
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        (option_filename, po::value<std::string>(), "name of dictionary file")
+        (option_wheel, po::value<std::string>(), "word wheel letters")
+        (option_central, po::value<char>(), "central letter (defaults to middle letter of word)")
+        (option_min_length, po::value<int>(), "minimum word length")
+        (option_part2, "include part 2");
+
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count(option_filename))
+            filename = vm[option_filename].as<std::string>();
+        if (vm.count(option_wheel))
+            letters = vm[option_wheel].as<std::string>();
+        if (vm.count(option_central))
+            central_letter = vm[option_central].as<char>();
+        if (vm.count(option_min_length))
+            min_length = vm[option_min_length].as<int>();
+        if (vm.count(option_part2))
+            do_part2 = true;
+
+        auto dict = load_dictionary(filename, min_length);
+        // part 1
+        word_wheel(dict, letters, central_letter);
+        // part 2
+        if (do_part2) {
+            std::cout << '\n';
+            find_max_word_count(dict, word_length);
+        }
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << '\n';
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
