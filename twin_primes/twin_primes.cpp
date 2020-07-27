d4d8b39999b61f61d230dd303e91091375fe8d62
Simon Saunders <simon.saunders@ntlworld.com>
2020-07-27 12:04:50 +0100

    Allow limits to be specified as command line arguments

--- twin_primes/twin_primes.cpp
+++ twin_primes/twin_primes.cpp
@@ -1,13 +1,31 @@
 #include <cstdint>
 #include <iostream>
+#include <string>
 #include <primesieve.hpp>
 
-int main() {
+void print_twin_prime_count(uint64_t limit) {
+    std::cout << "Number of twin prime pairs less than " << limit
+        << " is " << primesieve::count_twins(0, limit - 1) << '\n';
+}
+
+int main(int argc, char** argv) {
     std::cout.imbue(std::locale(""));
-    uint64_t limit = 10;
-    for (int power = 1; power < 12; ++power, limit *= 10) {
-        std::cout << "Number of twin prime pairs less than " << limit
-            << " is " << primesieve::count_twins(0, limit) << '\n';
+    if (argc > 1) {
+        // print number of twin prime pairs less than limits specified
+        // on the command line
+        for (int i = 1; i < argc; ++i) {
+            try {
+                print_twin_prime_count(std::stoull(argv[i]));
+            } catch (const std::exception& ex) {
+                std::cerr << "Cannot parse limit from '" << argv[i] << "'\n";
+            }
+        }
+    } else {
+        // if no limit was specified then show the number of twin prime
+        // pairs less than powers of 10 up to 100 billion
+        uint64_t limit = 10;
+        for (int power = 1; power < 12; ++power, limit *= 10)
+            print_twin_prime_count(limit);
     }
     return 0;
 }
--- twin_primes/twin_primes.rs
+++ twin_primes/twin_primes.rs
@@ -2,8 +2,9 @@
 // primal = "0.3"
 // num-format = "0.4"
 
-fn main() {
-    use num_format::{Locale, ToFormattedString};
+use num_format::{Locale, ToFormattedString};
+
+fn twin_prime_count_for_powers_of_ten(max_power: u32) {
     let mut count = 0;
     let mut previous = 0;
     let mut power = 1;
@@ -17,7 +18,7 @@ fn main() {
             );
             limit *= 10;
             power += 1;
-            if power > 10 {
+            if power > max_power {
                 break;
             }
         }
@@ -25,5 +26,36 @@ fn main() {
             count += 1;
         }
         previous = prime;
+    }
+}
+
+fn twin_prime_count(limit: usize) {
+    let mut count = 0;
+    let mut previous = 0;
+    for prime in primal::Primes::all().take_while(|x| *x < limit) {
+        if previous > 0 && prime == previous + 2 {
+            count += 1;
+        }
+        previous = prime;
+    }
+    println!(
+        "Number of twin prime pairs less than {} is {}",
+        limit.to_formatted_string(&Locale::en),
+        count.to_formatted_string(&Locale::en)
+    );
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        for i in 1..args.len() {
+            if let Ok(limit) = args[i].parse::<usize>() {
+                twin_prime_count(limit);
+            } else {
+                eprintln!("Cannot parse limit from string {}", args[i]);
+            }
+        }
+    } else {
+        twin_prime_count_for_powers_of_ten(10);
     }
 }
