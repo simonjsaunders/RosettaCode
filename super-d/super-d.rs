1557a6eac3b79f79dd6fbdbab512cb6fdbd61e09
Simon Saunders <simon.saunders@ntlworld.com>
2020-08-15 11:09:01 +0100

    Performance improvement

--- super-d/super-d.rs
+++ super-d/super-d.rs
@@ -2,15 +2,17 @@
 // rug = "1.9"
 
 fn print_super_d_numbers(d: u32, limit: u32) {
+    use rug::Assign;
     use rug::Integer;
-    use rug::ops::Pow;
 
     println!("First {} super-{} numbers:", limit, d);
     let digits = d.to_string().repeat(d as usize);
     let mut count = 0;
     let mut n = 1;
+    let mut s = Integer::new();
     while count < limit {
-        let s = Integer::from(n).pow(d) * d;
+        s.assign(Integer::u_pow_u(n, d));
+        s *= d;
         if s.to_string().contains(&digits) {
             print!("{} ", n);
             count += 1;
