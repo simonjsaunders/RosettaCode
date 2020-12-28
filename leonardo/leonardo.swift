f52e6c06c99772dacdd4fe2e9e3fb6a67d6a85db
Simon Saunders <simon.saunders@ntlworld.com>
2020-12-28 14:41:27 +0000

    Use default arguments

--- leonardo/leonardo.swift
+++ leonardo/leonardo.swift
@@ -3,7 +3,7 @@ struct Leonardo: Sequence, IteratorProtocol {
     private var n0: Int
     private var n1: Int
     
-    init(n0: Int, n1: Int, add: Int) {
+    init(n0: Int = 1, n1: Int = 1, add: Int = 1) {
         self.n0 = n0
         self.n1 = n1
         self.add = add
@@ -18,7 +18,7 @@ struct Leonardo: Sequence, IteratorProtocol {
 }
 
 print("First 25 Leonardo numbers:")
-print(Leonardo(n0: 1, n1: 1, add: 1).prefix(25).map{String($0)}.joined(separator: " "))
+print(Leonardo().prefix(25).map{String($0)}.joined(separator: " "))
 
 print("First 25 Fibonacci numbers:")
-print(Leonardo(n0: 0, n1: 1, add: 0).prefix(25).map{String($0)}.joined(separator: " "))
+print(Leonardo(n0: 0, add: 0).prefix(25).map{String($0)}.joined(separator: " "))
