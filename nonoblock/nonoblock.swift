218e76d9312e015c966508ab0ecca41c7cbe11cc
Simon Saunders <simon.saunders@ntlworld.com>
2021-01-03 09:18:56 +0000

    Use nested functions instead of class

--- nonoblock/nonoblock.swift
+++ nonoblock/nonoblock.swift
@@ -1,33 +1,14 @@
 import Foundation
 
-class Nonoblock {
-    let cells: Int
-    let blocks: [Int]
-    var positions: [Int]
-    var count: Int
-
-    private init(cells: Int, blocks: [Int]) {
-        self.cells = cells
-        self.blocks = blocks
-        positions = Array(repeating: 0, count: blocks.count)
-        count = 0
+func nonoblock(cells: Int, blocks: [Int]) {
+    print("\(cells) cells and blocks \(blocks):")
+    let totalBlockSize = blocks.reduce(0, +)
+    if cells < totalBlockSize + blocks.count - 1 {
+        print("no solution")
+        return
     }
 
-    class func printSolutions(cells: Int, blocks: [Int]) {
-        print("\(cells) cells and blocks \(blocks):")
-        var totalBlockSize = 0
-        for blockSize in blocks {
-            totalBlockSize += blockSize
-        }
-        if cells < totalBlockSize + blocks.count - 1 {
-            print("no solution")
-            return
-        }
-        let nb = Nonoblock(cells: cells, blocks: blocks)
-        nb.solve(cells: cells, index: 0, totalBlockSize: totalBlockSize)
-    }
-
-    private func solve(cells: Int, index: Int, totalBlockSize: Int) {
+    func solve(cells: Int, index: Int, totalBlockSize: Int) {
         if index == blocks.count {
             count += 1
             printSolution()
@@ -44,7 +25,7 @@ class Nonoblock {
         }
     }
 
-    private func printSolution() {
+    func printSolution() {
         var str = String(format: "%2d  ", count)
         var c = 0
         for i in 0..<blocks.count {
@@ -64,18 +45,22 @@ class Nonoblock {
         }
         print(str)
     }
+    
+    var positions = Array(repeating: 0, count: blocks.count)
+    var count = 0
+    solve(cells: cells, index: 0, totalBlockSize: totalBlockSize)
 }
 
-Nonoblock.printSolutions(cells: 5, blocks: [2, 1])
+nonoblock(cells: 5, blocks: [2, 1])
 print()
 
-Nonoblock.printSolutions(cells: 5, blocks: [])
+nonoblock(cells: 5, blocks: [])
 print()
 
-Nonoblock.printSolutions(cells: 10, blocks: [8])
+nonoblock(cells: 10, blocks: [8])
 print()
 
-Nonoblock.printSolutions(cells: 15, blocks: [2, 3, 2, 3])
+nonoblock(cells: 15, blocks: [2, 3, 2, 3])
 print()
 
-Nonoblock.printSolutions(cells: 5, blocks: [2, 3])
+nonoblock(cells: 5, blocks: [2, 3])
