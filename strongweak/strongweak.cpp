1de7aa1fb3a57407dacfc49710ec3d85ad64ec2c
Simon Saunders <simon.saunders@ntlworld.com>
2021-01-17 11:08:08 +0000

    Refactored C++ code for strong/weak primes

--- strongweak/strongweak.cpp
+++ strongweak/strongweak.cpp
@@ -4,47 +4,58 @@
 #include <sstream>
 #include "prime_sieve.hpp"
 
-int main() {
-    const int limit1 = 1000000;
-    const int limit2 = 10000000;
-    const int max_print[2] = { 36, 37 };
-    const int array_size = limit2 + 100;
+const int limit1 = 1000000;
+const int limit2 = 10000000;
 
-    // find the prime numbers up to array_size
-    prime_sieve sieve(array_size);
+class prime_info {
+public:
+    explicit prime_info(int max) : max_print(max) {}
+
+    void add_prime(int prime) {
+        ++count2;
+        if (prime < limit1)
+            ++count1;
+        if (count2 <= max_print) {
+            if (count2 > 1)
+                out << ' ';
+            out << prime;
+        }
+    }
+    
+    void print(std::ostream& os, const char* name) const {
+        os << "First " << max_print << " " << name << " primes: " << out.str() << '\n';
+        os << "Number of " << name << " primes below " << limit1 << ": " << count1 << '\n';
+        os << "Number of " << name << " primes below " << limit2 << ": " << count2 << '\n';
+    }
+private:
+    int max_print;
+    int count1 = 0;
+    int count2 = 0;
+    std::ostringstream out;
+};
+
+int main() {
+    prime_sieve sieve(limit2 + 100);
 
     // write numbers with groups of digits separated according to the system default locale
     std::cout.imbue(std::locale(""));
-    std::cout << std::fixed;
 
     // count and print strong/weak prime numbers
-    int count1[2] = { 0 };
-    int count2[2] = { 0 };
-    std::ostringstream out[2];
-    const char* strength[2] = { "strong", "weak" };
+    prime_info strong_primes(36);
+    prime_info weak_primes(37);
     int p1 = 2, p2 = 3;
     for (int p3 = 5; p2 < limit2; ++p3) {
         if (!sieve.is_prime(p3))
             continue;
         int diff = p1 + p3 - 2 * p2;
-        int index = diff < 0 ? 0 : (diff > 0 ? 1 : -1);
-        if (index != -1) {
-            ++count2[index];
-            if (p2 < limit1)
-                ++count1[index];
-            if (count2[index] <= max_print[index]) {
-                if (count2[index] > 1)
-                    out[index] << ' ';
-                out[index] << p2;
-            }
-        }
+        if (diff < 0)
+            strong_primes.add_prime(p2);
+        else if (diff > 0)
+            weak_primes.add_prime(p2);
         p1 = p2;
         p2 = p3;
     }
-    for (int i = 0; i < 2; ++i) {
-        std::cout << "First " << max_print[i] << " " << strength[i] << " primes: " << out[i].str() << '\n';
-        std::cout << "Number of " << strength[i] << " primes below " << limit1 << ": " << count1[i] << '\n';
-        std::cout << "Number of " << strength[i] << " primes below " << limit2 << ": " << count2[i] << '\n';
-    }
+    strong_primes.print(std::cout, "strong");
+    weak_primes.print(std::cout, "weak");
     return 0;
 }
