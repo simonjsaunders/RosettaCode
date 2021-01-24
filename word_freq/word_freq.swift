3fafab88a7785e5ee0c679e2a62c860826508f30
Simon Saunders <simon.saunders@ntlworld.com>
2021-01-24 16:09:45 +0000

    Reduce memory usage

--- word_freq/word_freq.swift
+++ word_freq/word_freq.swift
@@ -6,13 +6,11 @@ func printTopWords(path: String, count: Int) throws {
     var dict = Dictionary<String, Int>()
     // split text into words, convert to lowercase and store word counts in dict
     let regex = try NSRegularExpression(pattern: "\\w+")
-    for match in regex.matches(in: text, range: NSRange(text.startIndex..., in: text)) {
+    regex.enumerateMatches(in: text, range: NSRange(text.startIndex..., in: text)) {
+        (match, _, _) in
+        guard let match = match else { return }
         let word = String(text[Range(match.range, in: text)!]).lowercased()
-        if let n = dict[word] {
-            dict[word] = n + 1
-        } else {
-            dict[word] = 1
-        }
+        dict[word, default: 0] += 1
     }
     // sort words by number of occurrences
     let wordCounts = dict.sorted(by: {$0.1 > $1.1})
