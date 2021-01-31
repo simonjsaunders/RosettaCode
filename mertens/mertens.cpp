2017ec1dc3b5fd461234c864b96e240363c1053d
Simon Saunders <simon.saunders@ntlworld.com>
2021-01-31 17:02:41 +0000

    Simplified C++ Mertens code

--- mertens/mertens.cpp
+++ mertens/mertens.cpp
@@ -1,55 +1,41 @@
 #include <iomanip>
 #include <iostream>
-#include <map>
+#include <vector>
 
-class mertens_calculator {
-public:
-    int mertens_number(int);
-private:
-    std::map<int, int> cache_;
-};
-
-int mertens_calculator::mertens_number(int n) {
-    auto i = cache_.find(n);
-    if (i != cache_.end())
-        return i->second;
-    int m = 1;
-    for (int k = 2; k <= n; ++k)
-        m -= mertens_number(n/k);
-    cache_.emplace(n, m);
+std::vector<int> mertens_numbers(int max) {
+    std::vector<int> m(max + 1, 1);
+    for (int n = 2; n <= max; ++n) {
+        for (int k = 2; k <= n; ++k)
+            m[n] -= m[n / k];
+    }
     return m;
 }
 
-void print_mertens_numbers(mertens_calculator& mc, int count) {
-    int column = 0;
-    for (int i = 0; i < count; ++i) {
+int main() {
+    const int max = 1000;
+    auto m(mertens_numbers(max));
+    std::cout << "First 199 Mertens numbers:\n";
+    for (int i = 0, column = 0; i < 200; ++i) {
         if (column > 0)
             std::cout << ' ';
         if (i == 0)
             std::cout << "  ";
         else
-            std::cout << std::setw(2) << mc.mertens_number(i);
+            std::cout << std::setw(2) << m[i];
         ++column;
         if (column == 20) {
             std::cout << '\n';
             column = 0;
         }
     }
-}
-
-int main() {
-    mertens_calculator mc;
-    std::cout << "First 199 Mertens numbers:\n";
-    print_mertens_numbers(mc, 200);
     int zero = 0, cross = 0, previous = 0;
-    for (int i = 1; i <= 1000; ++i) {
-        int m = mc.mertens_number(i);
-        if (m == 0) {
+    for (int i = 1; i <= max; ++i) {
+        if (m[i] == 0) {
             ++zero;
             if (previous != 0)
                 ++cross;
         }
-        previous = m;
+        previous = m[i];
     }
     std::cout << "M(n) is zero " << zero << " times for 1 <= n <= 1000.\n";
     std::cout << "M(n) crosses zero " << cross << " times for 1 <= n <= 1000.\n";
