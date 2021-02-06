00a797005076a29fb44a7bd6f12ea60bcad88d17
Simon Saunders <simon.saunders@ntlworld.com>
2021-02-06 09:44:46 +0000

    C++ solution for 'Square-free integers' is faster without using a prime sieve

--- square_free/square_free.cpp
+++ square_free/square_free.cpp
@@ -1,26 +1,28 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
-#include "prime_sieve.hpp"
 
 using integer = uint64_t;
 
-bool square_free(const prime_sieve& sieve, integer n) {
+bool square_free(integer n) {
     if (n % 4 == 0)
         return false;
     for (integer p = 3; p * p <= n; p += 2) {
-        if (sieve.is_prime(p) && n % (p * p) == 0)
-            return false;
+        integer count = 0;
+        for (; n % p == 0; n /= p) {
+            if (++count > 1)
+                return false;
+        }
     }
     return true;
 }
 
-void print_square_free_numbers(const prime_sieve& sieve, integer from, integer to) {
+void print_square_free_numbers(integer from, integer to) {
     std::cout << "Square-free numbers between " << from
         << " and " << to << ":\n";
     std::string line;
     for (integer i = from; i <= to; ++i) {
-        if (square_free(sieve, i)) {
+        if (square_free(i)) {
             if (!line.empty())
                 line += ' ';
             line += std::to_string(i);
@@ -34,10 +36,10 @@ void print_square_free_numbers(const prime_sieve& sieve, integer from, integer t
         std::cout << line << '\n';
 }
 
-void print_square_free_count(const prime_sieve& sieve, integer from, integer to) {
+void print_square_free_count(integer from, integer to) {
     integer count = 0;
     for (integer i = from; i <= to; ++i) {
-        if (square_free(sieve, i))
+        if (square_free(i))
             ++count;
     }
     std::cout << "Number of square-free numbers between "
@@ -45,13 +47,12 @@ void print_square_free_count(const prime_sieve& sieve, integer from, integer to)
 }
 
 int main() {
-    prime_sieve sieve(1000001);
-    print_square_free_numbers(sieve, 1, 145);
-    print_square_free_numbers(sieve, 1000000000000LL, 1000000000145LL);
-    print_square_free_count(sieve, 1, 100);
-    print_square_free_count(sieve, 1, 1000);
-    print_square_free_count(sieve, 1, 10000);
-    print_square_free_count(sieve, 1, 100000);
-    print_square_free_count(sieve, 1, 1000000);
+    print_square_free_numbers(1, 145);
+    print_square_free_numbers(1000000000000LL, 1000000000145LL);
+    print_square_free_count(1, 100);
+    print_square_free_count(1, 1000);
+    print_square_free_count(1, 10000);
+    print_square_free_count(1, 100000);
+    print_square_free_count(1, 1000000);
     return 0;
 }
