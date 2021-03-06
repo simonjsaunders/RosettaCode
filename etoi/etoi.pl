e10ca34b6fb314d247c3d16670ec37362df82d70
Simon Saunders <simon.saunders@ntlworld.com>
2021-03-06 17:51:26 +0000

    Use forall instead of failure-driven loop

--- etoi/etoi.pl
+++ etoi/etoi.pl
@@ -2,15 +2,13 @@
 
 main:-
     load_dictionary_from_file("unixdict.txt", 6),
-    dictionary_word(Word1),
-    string_chars(Word1, Chars1),
-    memberchk('e', Chars1),
-    replace('e', 'i', Chars1, Chars2),
-    string_chars(Word2, Chars2),
-    dictionary_word(Word2),
-    writef('%10l -> %w\n', [Word1, Word2]),
-    fail.
-main.
+    forall((dictionary_word(Word1),
+            string_chars(Word1, Chars1),
+            memberchk('e', Chars1),
+            replace('e', 'i', Chars1, Chars2),
+            string_chars(Word2, Chars2),
+            dictionary_word(Word2)),
+            writef('%10l -> %w\n', [Word1, Word2])).
 
 load_dictionary_from_file(File, Min_length):-
     open(File, read, Stream),
