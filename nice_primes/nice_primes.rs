464c09f1870e3de567d658a5906637a4e8fc7cfe
Simon Saunders <simon.saunders@ntlworld.com>
2021-03-13 15:15:43 +0000

    Use a sieve instead of generating all primes

--- nice_primes/nice_primes.rs
+++ nice_primes/nice_primes.rs
@@ -10,9 +10,9 @@ fn digital_root(n: u64) -> u64 {
 }
 
 fn nice_primes(from: usize, to: usize) {
-    primal::Primes::all()
-        .skip_while(|x| x < &from)
-        .take_while(|x| x < &to)
+    primal::Sieve::new(to)
+        .primes_from(from)
+        .take_while(|x| *x < to)
         .filter(|x| primal::is_prime(digital_root(*x as u64)))
         .for_each(|x| println!("{}", x));
 }
