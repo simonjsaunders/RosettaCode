53eefef4ca8195128ce9c193d29a731124fab270
Simon Saunders <simon.saunders@ntlworld.com>
2021-05-23 11:42:28 +0100

    Changed output function to take sign as parameter.

--- permswap/permswap.rs
+++ permswap/permswap.rs
@@ -1,13 +1,14 @@
 // Implementation of Heap's algorithm.
 // See https://en.wikipedia.org/wiki/Heap%27s_algorithm#Details_of_the_algorithm
-fn generate<T, F>(a: &mut [T], mut output: F)
+fn generate<T, F>(a: &mut [T], output: F)
 where
-    F: FnMut(&[T]),
+    F: Fn(&[T], isize),
 {
     let n = a.len();
     let mut c = vec![0; n];
     let mut i = 1;
-    output(a);
+    let mut sign = 1;
+    output(a, sign);
     while i < n {
         if c[i] < i {
             if (i & 1) == 0 {
@@ -15,7 +16,8 @@ where
             } else {
                 a.swap(c[i], i);
             }
-            output(a);
+            sign = -sign;
+            output(a, sign);
             c[i] += 1;
             i = 1;
         } else {
@@ -25,21 +27,16 @@ where
     }
 }
 
-fn print_permutations<T: std::fmt::Debug>(a: &mut [T]) {
-    let mut even = true;
-    let p = move |x: &[T]| {
-        println!("{:?} {}", x, if even { "+1" } else { "-1" });
-        even = !even;
-    };
-    generate(a, p);
+fn print_permutation<T: std::fmt::Debug>(a: &[T], sign: isize) {
+    println!("{:?} {}", a, sign);
 }
 
 fn main() {
     println!("Permutations and signs for three items:");
     let mut a = vec![0, 1, 2];
-    print_permutations(&mut a);
+    generate(&mut a, print_permutation);
 
     println!("\nPermutations and signs for four items:");
     let mut b = vec![0, 1, 2, 3];
-    print_permutations(&mut b);
+    generate(&mut b, print_permutation);
 }
--- permswap/permswap.swift
+++ permswap/permswap.swift
@@ -1,10 +1,11 @@
 // Implementation of Heap's algorithm.
 // See https://en.wikipedia.org/wiki/Heap%27s_algorithm#Details_of_the_algorithm
-func generate<T>(array: inout [T], output: (_: [T]) -> Void) {
+func generate<T>(array: inout [T], output: (_: [T], _: Int) -> Void) {
     let n = array.count
     var c = Array(repeating: 0, count: n)
     var i = 1
-    output(array)
+    var sign = 1
+    output(array, sign)
     while i < n {
         if c[i] < i {
             if (i & 1) == 0 {
@@ -12,7 +13,8 @@ func generate<T>(array: inout [T], output: (_: [T]) -> Void) {
             } else {
                 array.swapAt(c[i], i)
             }
-            output(array)
+            sign = -sign
+            output(array, sign)
             c[i] += 1
             i = 1
         } else {
@@ -22,19 +24,14 @@ func generate<T>(array: inout [T], output: (_: [T]) -> Void) {
     }
 }
 
-func printPermutations<T>(array: inout [T]) {
-    var even = true
-    func p(array: [T]) {
-        print("\(array) \(even ? "+1" : "-1")")
-        even = !even
-    }
-    generate(array: &array, output: p)
+func printPermutation<T>(array: [T], sign: Int) {
+    print("\(array) \(sign)")
 }
 
 print("Permutations and signs for three items:")
 var a = [0, 1, 2]
-printPermutations(array: &a)
+generate(array: &a, output: printPermutation)
 
 print("\nPermutations and signs for four items:")
 var b = [0, 1, 2, 3]
-printPermutations(array: &b)
+generate(array: &b, output: printPermutation)
