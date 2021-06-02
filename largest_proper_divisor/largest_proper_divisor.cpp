bbcc6454ff1727a6499cf5435d3e99061e5107d6
Simon Saunders <simon.saunders@ntlworld.com>
2021-06-02 11:39:53 +0100

    Added assert

--- largest_proper_divisor/largest_proper_divisor.cpp
+++ largest_proper_divisor/largest_proper_divisor.cpp
@@ -1,7 +1,9 @@
+#include <cassert>
 #include <iomanip>
 #include <iostream>
 
 int largest_proper_divisor(int n) {
+    assert(n > 0);
     if ((n & 1) == 0)
         return n >> 1;
     for (int p = 3; p * p <= n; p += 2) {
