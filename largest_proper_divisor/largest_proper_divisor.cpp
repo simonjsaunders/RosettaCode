6f4a0640f2c7096af6db1fafcc08946e565a75db
Simon Saunders <simon.saunders@ntlworld.com>
2021-06-02 10:09:14 +0100

    Added C++ solution for task 'Largest proper divisor of n'

---
+++ largest_proper_divisor/largest_proper_divisor.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <iostream>
+
+int largest_proper_divisor(int n) {
+    if ((n & 1) == 0)
+        return n >> 1;
+    for (int p = 3; p * p <= n; p += 2) {
+        if (n % p == 0)
+            return n / p;
+    }
+    return 1;
+}
+
+int main() {
+    for (int n = 1; n < 101; ++n) {
+        std::cout << std::setw(2) << largest_proper_divisor(n)
+            << (n % 10 == 0 ? '\n' : ' ');
+    }
+}
