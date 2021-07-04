7ddb667af433d2196f5efae8e9ae7a4eb4ad9f65
Simon Saunders <simon.saunders@ntlworld.com>
2021-07-04 16:20:55 +0100

    Simplified code

--- harmonic_series/harmonic_series.pl
+++ harmonic_series/harmonic_series.pl
@@ -8,7 +8,7 @@ main:-
 print_harmonic_series(N):-
     harmonic_series(N, S),
     writef('First %t harmonic numbers:\n', [N]),
-    forall(nth1(I, S, T),
+    forall(member(h(I, T), S),
            (Num is numerator(T),
             Denom is denominator(T),
             writef('%3r. %t/%t\n', [I, Num, Denom]))).
@@ -21,24 +21,22 @@ print_nth_harmonic_number(N):-
 
 print_first_harmonic_greater_than(N):-
     harmonic_first(H),
-    print_first_harmonic_greater_than(1, N, 1, H).
+    print_first_harmonic_greater_than(1, N, H).
 
-print_first_harmonic_greater_than(N, L, _, _):-
+print_first_harmonic_greater_than(N, L, _):-
     N > L,
     !.
-print_first_harmonic_greater_than(N, L, P, H):-
-    H = h(_, T),
+print_first_harmonic_greater_than(N, L, H):-
+    H = h(P, T),
     T > N,
     !,
     writef('Position of first term >%3r: %t\n', [N, P]),
     N1 is N + 1,
-    P1 is P + 1,
     harmonic_next(H, H1),
-    print_first_harmonic_greater_than(N1, L, P1, H1).
-print_first_harmonic_greater_than(N, L, P, H):-
-    P1 is P + 1,
+    print_first_harmonic_greater_than(N1, L, H1).
+print_first_harmonic_greater_than(N, L, H):-
     harmonic_next(H, H1),
-    print_first_harmonic_greater_than(N, L, P1, H1).
+    print_first_harmonic_greater_than(N, L, H1).
 
 harmonic_first(h(1, 1)).
 
@@ -50,19 +48,18 @@ harmonic_series(N, S):-
     harmonic_first(H),
     harmonic_series(N, S, H).
 
-harmonic_series(1, [T], h(_, T)):-!.
-harmonic_series(N1, [T|Terms], H1):-
-    N2 is N1 - 1,
-    H1 = h(_, T),
+harmonic_series(N, [H], H):-
+    H = h(N, _),
+    !.
+harmonic_series(N, [H1|Terms], H1):-
     harmonic_next(H1, H2),
-    harmonic_series(N2, Terms, H2).
+    harmonic_series(N, Terms, H2).
 
 nth_harmonic_number(N, T):-
     harmonic_first(H),
     nth_harmonic_number(N, T, H).
 
-nth_harmonic_number(1, T, h(_, T)):-!.
-nth_harmonic_number(N1, T, H1):-
-    N2 is N1 - 1,
+nth_harmonic_number(N, T, h(N, T)):-!.
+nth_harmonic_number(N, T, H1):-
     harmonic_next(H1, H2),
-    nth_harmonic_number(N2, T, H2).
+    nth_harmonic_number(N, T, H2).
