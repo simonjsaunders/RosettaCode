fa8f89c31accdc0a923ffbb137c0afeb4a4c0761
Simon Saunders <simon.saunders@ntlworld.com>
2021-07-10 07:12:24 +0100

    Multi base primes C++ solution extended to base 62

--- multi_base_primes/multi_base_primes.cpp
+++ multi_base_primes/multi_base_primes.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <vector>
 
@@ -25,8 +27,7 @@ std::vector<bool> prime_sieve(uint64_t limit) {
     return sieve;
 }
 
-template <typename T>
-void print(std::ostream& out, const std::vector<T>& v) {
+template <typename T> void print(std::ostream& out, const std::vector<T>& v) {
     if (!v.empty()) {
         out << '[';
         auto i = v.begin();
@@ -38,41 +39,58 @@ void print(std::ostream& out, const std::vector<T>& v) {
 }
 
 std::string to_string(const std::vector<unsigned int>& v) {
-    static constexpr char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static constexpr char digits[] =
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     std::string str;
     for (auto i : v)
         str += digits[i];
     return str;
 }
 
-class multi_base_primes {
-public:
-    explicit multi_base_primes(unsigned int depth);
-    void run();
+bool increment(std::vector<unsigned int>& digits, unsigned int max_base) {
+    auto i = digits.rbegin();
+    for (; i != digits.rend() && *i + 1 == max_base; ++i)
+        *i = 0;
+    if (i == digits.rend())
+        return false;
+    ++*i;
+    return true;
+}
 
-private:
-    void process(const std::vector<unsigned int>& indices);
-    void nested_for(std::vector<unsigned int>& indices, unsigned int level);
-    static const unsigned int max_base = 36;
-    unsigned int max_depth;
-    std::vector<bool> sieve;
-    unsigned int most_bases = 0;
-    std::vector<std::pair<std::vector<unsigned int>, std::vector<unsigned int>>>
-        max_strings;
-};
-
-multi_base_primes::multi_base_primes(unsigned int depth)
-    : max_depth(depth),
-      sieve(prime_sieve(static_cast<uint64_t>(std::pow(max_base, depth)))) {}
-
-void multi_base_primes::run() {
-    for (unsigned int depth = 1; depth <= max_depth; ++depth) {
-        std::cout << depth
-                  << " character strings which are prime in most bases: ";
-        max_strings.clear();
-        most_bases = 0;
-        std::vector<unsigned int> indices(depth, 0);
-        nested_for(indices, 0);
+void multi_base_primes(unsigned int max_base, unsigned int max_length) {
+    auto sieve =
+        prime_sieve(static_cast<uint64_t>(std::pow(max_base, max_length)));
+    for (unsigned int length = 1; length <= max_length; ++length) {
+        std::cout << length
+                  << "-character strings which are prime in most bases: ";
+        unsigned int most_bases = 0;
+        std::vector<
+            std::pair<std::vector<unsigned int>, std::vector<unsigned int>>>
+            max_strings;
+        std::vector<unsigned int> digits(length, 0);
+        digits[0] = 1;
+        do {
+            auto max = std::max_element(digits.begin(), digits.end());
+            unsigned int min_base = 2;
+            if (max != digits.end())
+                min_base = std::max(min_base, *max + 1);
+            if (most_bases > max_base - min_base)
+                continue;
+            std::vector<unsigned int> bases;
+            for (unsigned int b = min_base; b <= max_base; ++b) {
+                uint64_t n = 0;
+                for (auto d : digits)
+                    n = n * b + d;
+                if (sieve[n])
+                    bases.push_back(b);
+            }
+            if (bases.size() > most_bases) {
+                most_bases = bases.size();
+                max_strings.clear();
+            }
+            if (bases.size() == most_bases)
+                max_strings.emplace_back(digits, bases);
+        } while (increment(digits, max_base));
         std::cout << most_bases << '\n';
         for (const auto& m : max_strings) {
             std::cout << to_string(m.first) << " -> ";
@@ -83,43 +101,23 @@ void multi_base_primes::run() {
     }
 }
 
-void multi_base_primes::process(const std::vector<unsigned int>& indices) {
-    auto max = std::max_element(indices.begin(), indices.end());
-    unsigned int min_base = 2;
-    if (max != indices.end())
-        min_base = std::max(min_base, *max + 1);
-    if (most_bases > max_base - min_base)
-        return;
-    std::vector<unsigned int> bases;
-    for (unsigned int b = min_base; b <= max_base; ++b) {
-        uint64_t n = 0;
-        for (auto i : indices)
-            n = n * b + i;
-        if (sieve[n])
-            bases.push_back(b);
+int main(int argc, char** argv) {
+    unsigned int max_base = 36;
+    unsigned int max_length = 4;
+    for (int arg = 1; arg + 1 < argc; ++arg) {
+        if (strcmp(argv[arg], "-max_base") == 0)
+            max_base = strtoul(argv[++arg], nullptr, 10);
+        else if (strcmp(argv[arg], "-max_length") == 0)
+            max_length = strtoul(argv[++arg], nullptr, 10);
     }
-    if (bases.size() > most_bases) {
-        most_bases = bases.size();
-        max_strings.clear();
+    if (max_base > 62) {
+        std::cerr << "Max base cannot be greater than 62.\n";
+        return EXIT_FAILURE;
     }
-    if (bases.size() == most_bases)
-        max_strings.emplace_back(indices, bases);
-}
-
-void multi_base_primes::nested_for(std::vector<unsigned int>& indices,
-                                   unsigned int level) {
-    if (level == indices.size()) {
-        process(indices);
-    } else {
-        indices[level] = (level == 0) ? 1 : 0;
-        while (indices[level] < max_base) {
-            nested_for(indices, level + 1);
-            ++indices[level];
-        }
+    if (max_base < 2) {
+        std::cerr << "Max base cannot be less than 2.\n";
+        return EXIT_FAILURE;
     }
-}
-
-int main() {
-    multi_base_primes mbp(6);
-    mbp.run();
+    multi_base_primes(max_base, max_length);
+    return EXIT_SUCCESS;
 }
