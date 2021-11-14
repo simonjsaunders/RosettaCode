0fce751790e6fe9bcb5f154002d4718c6ca86757
Simon Saunders <simon.saunders@ntlworld.com>
2021-11-14 14:10:20 +0000

    Performance improvement

--- word_ladder/word_ladder.swift
+++ word_ladder/word_ladder.swift
@@ -1,32 +1,36 @@
 import Foundation
 
-func oneAway(string1: String, string2: String) -> Bool {
+func oneAway(string1: [Character], string2: [Character]) -> Bool {
     if string1.count != string2.count {
         return false
     }
     var result = false
-    for (c1, c2) in zip(string1, string2) {
-        if c1 != c2 {
+    var i = 0
+    while i < string1.count {
+        if string1[i] != string2[i] {
             if result {
                 return false
             }
             result = true
         }
+        i += 1
     }
     return result
 }
 
-func wordLadder(words: [String], from: String, to: String) {
-    var poss = words.filter{$0.count == from.count}
-    var queue: [[String]] = [[from]]
+func wordLadder(words: [[Character]], from: String, to: String) {
+    let fromCh = Array(from)
+    let toCh = Array(to)
+    var poss = words.filter{$0.count == fromCh.count}
+    var queue: [[[Character]]] = [[fromCh]]
     while !queue.isEmpty {
         var curr = queue[0]
         let last = curr[curr.count - 1]
         queue.removeFirst()
         let next = poss.filter{oneAway(string1: $0, string2: last)}
-        if next.contains(to) {
-            curr.append(to)
-            print(curr.joined(separator: " -> "))
+        if next.contains(toCh) {
+            curr.append(toCh)
+            print(curr.map{String($0)}.joined(separator: " -> "))
             return
         }
         poss.removeAll(where: {next.contains($0)})
@@ -43,10 +47,15 @@ do {
     let words = try String(contentsOfFile: "unixdict.txt", encoding: String.Encoding.ascii)
         .components(separatedBy: "\n")
         .filter{!$0.isEmpty}
+        .map{Array($0)}
     wordLadder(words: words, from: "man", to: "boy")
     wordLadder(words: words, from: "girl", to: "lady")
     wordLadder(words: words, from: "john", to: "jane")
     wordLadder(words: words, from: "child", to: "adult")
+    wordLadder(words: words, from: "cat", to: "dog")
+    wordLadder(words: words, from: "lead", to: "gold")
+    wordLadder(words: words, from: "white", to: "black")
+    wordLadder(words: words, from: "bubble", to: "tickle")
 } catch {
     print(error.localizedDescription)
 }
