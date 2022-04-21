87691baa717565fc179f10c8b275ebe2309a516d
Simon Saunders <simon.saunders@ntlworld.com>
2022-04-21 10:04:54 +0100

    Use iterators instead of loops

--- cullen_woodall/cullen_woodall.rs
+++ cullen_woodall/cullen_woodall.rs
@@ -1,8 +1,8 @@
 // [dependencies]
 // rug = "1.15.0"
 
-use rug::Integer;
 use rug::integer::IsPrime;
+use rug::Integer;
 
 fn cullen_number(n: u32) -> Integer {
     let num = Integer::from(n);
@@ -16,47 +16,34 @@ fn woodall_number(n: u32) -> Integer {
 
 fn main() {
     println!("First 20 Cullen numbers:");
-    for n in 1..21 {
-        if n > 1 {
-            print!(" ");
-        }
-        print!("{}", cullen_number(n));
-    }
+    let cullen: Vec<String> = (1..21)
+        .map(|x| cullen_number(x).to_string_radix(10))
+        .collect();
+    println!("{}", cullen.join(" "));
 
-    println!("\n\nFirst 20 Woodall numbers:");
-    for n in 1..21 {
-        if n > 1 {
-            print!(" ");
-        }
-        print!("{}", woodall_number(n));
-    }
+    println!("\nFirst 20 Woodall numbers:");
+    let woodall: Vec<String> = (1..21)
+        .map(|x| woodall_number(x).to_string_radix(10))
+        .collect();
+    println!("{}", woodall.join(" "));
 
-    println!("\n\nFirst 5 Cullen primes in terms of n:");
-    let mut n = 1;
-    let mut count = 0;
-    while count != 5 {
-        if cullen_number(n).is_probably_prime(25) != IsPrime::No {
-            if count > 0 {
-                print!(" ");
-            }
-            print!("{}", n);
-            count += 1;
-        }
-        n += 1;
-    }
+    println!("\nFirst 5 Cullen primes in terms of n:");
+    let cullen_primes: Vec<String> = (1..)
+        .filter_map(|x| match cullen_number(x).is_probably_prime(25) {
+            IsPrime::No => None,
+            _ => Some(x.to_string()),
+        })
+        .take(5)
+        .collect();
+    println!("{}", cullen_primes.join(" "));
 
-    println!("\n\nFirst 5 Woodall primes in terms of n:");
-    n = 1;
-    count = 0;
-    while count != 12 {
-        if woodall_number(n).is_probably_prime(25) != IsPrime::No {
-            if count > 0 {
-                print!(" ");
-            }
-            print!("{}", n);
-            count += 1;
-        }
-        n += 1;
-    }
-    println!();
+    println!("\nFirst 12 Woodall primes in terms of n:");
+    let woodall_primes: Vec<String> = (1..)
+        .filter_map(|x| match woodall_number(x).is_probably_prime(25) {
+            IsPrime::No => None,
+            _ => Some(x.to_string()),
+        })
+        .take(12)
+        .collect();
+    println!("{}", woodall_primes.join(" "));
 }
